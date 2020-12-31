eb1094f9d5eb3e19e043d3582f0125ee3debe0bd
Lky777 <73606056+Lky777@users.noreply.github.com>
2020-12-31 20:37:56 +0800

    Add files via upload

--- ocservauto.sh
+++ ocservauto.sh
@@ -313,12 +313,12 @@ function get_Custom_configuration_2(){
         fast_Default_Ask "$OC_version_latest is the latest,but default version is recommended.Which to choose?" "$Default_oc_version" "oc_version"
     }
 #which port to use for verification 选择验证端口
-    fast_Default_Ask "Which port to use for verification?(Tcp-Port)" "9999" "ocserv_tcpport_set"
+    fast_Default_Ask "Which port to use for verification?(Tcp-Port)" "8080" "ocserv_tcpport_set"
 #tcp-port only or not 是否仅仅使用tcp端口，即是否禁用udp
     fast_Default_Ask "Only use tcp-port or not?(y/n)" "n" "only_tcp_port"
 #which port to use for data transmission 选择udp端口 即专用数据传输的udp端口
     if [ "$only_tcp_port" = "n" ]; then
-        fast_Default_Ask "Which port to use for data transmission?(Udp-Port)" "9999" "ocserv_udpport_set"
+        fast_Default_Ask "Which port to use for data transmission?(Udp-Port)" "8080" "ocserv_udpport_set"
     fi
 #boot from the start 是否开机自起
     fast_Default_Ask "Start ocserv when system is started?(y/n)" "y" "ocserv_boot_start"
@@ -604,8 +604,8 @@ _EOF_
 #modify config file 设定相关参数
 function set_ocserv_conf(){
 #default vars
-    ocserv_tcpport_set=${ocserv_tcpport_set:-9999}
-    ocserv_udpport_set=${ocserv_udpport_set:-9999}
+    ocserv_tcpport_set=${ocserv_tcpport_set:-999}
+    ocserv_udpport_set=${ocserv_udpport_set:-1999}
     save_user_vars=${save_user_vars:-n}
     ocserv_boot_start=${ocserv_boot_start:-y}
     only_tcp_port=${only_tcp_port:-n}
